303e78202e2f182811955cfb66cf113ed053046c
Nozzyソ <94677706+NozyxN@users.noreply.github.com>
2021-11-21 10:26:31 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,25 +1,19 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
+return `PAYMENT BY NOXZY
 		
-*さ E-MONEY*
+*• E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 0813 2387 0860
+		• N**ZY
 2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ 0813 2387 0860
+		• N**ZY
 3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 0813 2387 0860
+		• N**ZY
 
-*さ PULSA*
-
-Tidak tersedia
+*• PULSA*
+      • 081323870860
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
